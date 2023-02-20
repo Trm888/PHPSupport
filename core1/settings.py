ab8e1fafcbb268ff757369ad9e9a3d5e5e3e337c
tram888 <bigtram888@gmail.com>
2023-02-20 21:13:23 +0900

    Edit settings.py

--- core1/settings.py
+++ core1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cief1jw_&^h#v+gv=g*pu5qx+cg94(cqehq=#pbgje!3z2udj4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'phpsupport-production.up.railway.app']
 
 
 # Application definition
